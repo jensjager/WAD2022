a003215439c4563b43325f86ab753f0aae3ded6b
JanMa <jan.markus.rokka@ut.ee>
2022-11-01 17:22:45 +0200

    Moved the dropdown element entirely into global.js (to add, use id="dropdown-insert-here").

--- res/js/global.js
+++ res/js/global.js
@@ -6,3 +6,17 @@ function toggleDropdown() {
     else
         dropdown.style.display = "flex";
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    var dropdownHTML = `
+    <div class="dropdown">
+        <img onclick="toggleDropdown()" src="https://i.ibb.co/8DY3YCm/user-avatar.png" alt="User avatar">
+        <div id="dropdown-content">
+            <a href="#">John Doe</a>
+            <a href="#">john.doe@ut.ee</a>
+            <a href="../login-page/login.html">logout</a>
+        </div>
+    </div>
+`
+document.querySelector('#dropdown-insert-here').insertAdjacentHTML("afterend", dropdownHTML);
+});
