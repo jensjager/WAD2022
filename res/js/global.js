2bd260c9232fb801cb26098eaf398e0f45b6150a
Taavi Eistre <taavi.eistre@ut.ee>
2022-11-01 20:53:08 +0200

    Small refactor to dropdown to better suit lecture and practicals content.

--- res/js/global.js
+++ res/js/global.js
@@ -7,16 +7,29 @@ function toggleDropdown() {
         dropdown.style.display = "flex";
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    var dropdownHTML = `
-    <div class="dropdown">
-        <img onclick="toggleDropdown()" src="https://i.ibb.co/8DY3YCm/user-avatar.png" alt="User avatar">
-        <div id="dropdown-content">
-            <a href="#">John Doe</a>
-            <a href="#">john.doe@ut.ee</a>
-            <a href="../login-page/login.html">logout</a>
-        </div>
-    </div>
-`
-document.querySelector('#dropdown-insert-here').insertAdjacentHTML("afterend", dropdownHTML);
+window.addEventListener("load", function () {
+    let img = document.createElement("img");
+    img.src = "https://i.ibb.co/8DY3YCm/user-avatar.png";
+    img.alt = "User avatar";
+    img.addEventListener("click", toggleDropdown);
+
+    let div = document.createElement("div");
+    div.id = "dropdown-content"
+
+    let a1 = document.createElement("a")
+    a1.href = "#";
+    a1.innerHTML = "John Doe";
+
+    let a2 = document.createElement("a")
+    a2.href = "#";
+    a2.innerHTML = "john.doe@ut.ee";
+
+    let a3 = document.createElement("a");
+    a3.href = "../login-page/login.html"
+    if (window.location.pathname.split("/").pop() === "index.html")
+        a3.href = "./login-page/login.html";
+    a3.innerHTML = "logout";
+
+    div.append(a1, a2, a3);
+    document.querySelector("#dropdown").append(img, div);
 });
