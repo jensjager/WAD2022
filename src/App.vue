419f253965cab94ad0e9195da4963a443133083a
Taavi Eistre <taavi.eistre@ut.ee>
2022-11-16 16:10:09 +0200

    Created Header component and changed router/view names

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Header></Header>
   <router-view/>
 </template>
 
@@ -14,17 +11,12 @@
   text-align: center;
   color: #2c3e50;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
+
+<script>
+import Header from "@/components/Header";
+
+export default {
+  components: {Header}
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/signup">Sign up</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+nav {
+  position: fixed;
+  left: 0;
+  top: 0;
+  display: flex;
+  width: 94vw;
+  height: 10vh;
+  background-color: gray;
+  border-radius: 15px;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 3vw 0 3vw;
+  font-size: x-large;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
